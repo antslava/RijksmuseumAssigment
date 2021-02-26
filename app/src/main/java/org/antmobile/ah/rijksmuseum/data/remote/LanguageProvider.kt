0bb56b8622b21c60678f3ea1a87a5d6c7dfba031
Viacheslav Antonenko <viacheslav.antonenko@antmobile.org>
2021-02-26 03:13:49 +0100

    Define retrofit API interface for Rijksmuseum API

---
+++ app/src/main/java/org/antmobile/ah/rijksmuseum/data/remote/LanguageProvider.kt
@@ -0,0 +1,27 @@
+package org.antmobile.ah.rijksmuseum.data.remote
+
+import android.content.Context
+import org.antmobile.ah.rijksmuseum.R
+import java.util.*
+
+class LanguageProvider(
+    private val appContext: Context
+) {
+
+    fun provideSupportedLanguage(): Language {
+        val language = appContext.getString(R.string.language)
+        return Language.values().firstOrNull {
+            it.name.equals(language, true)
+        } ?: Language.EN
+    }
+
+}
+
+enum class Language {
+    EN, NL;
+
+    override fun toString(): String = super.toString().toLowerCase(Locale.ENGLISH)
+
+
+}
+
